300cac49178ebc574b687f321c7506d39d2eb80a
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-10-02 15:45:36 +0900

    初期化処理

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,7 +1,17 @@
 #include "Player.h"
+#include "cassert"
 
 void Player::Initialize(Model* model, uint32_t textureHandle) {
 
+	//NULLポインタチェック
+	assert(model);
+
+	//引数で受け取ったデータをメンバ変数に記録
+	model_ = model;
+	textureHandle_ = textureHandle;
+
+	//ワールド変換の初期化
+	worldTransform_.Initialize();
 }
 
 void Player::Update() {
