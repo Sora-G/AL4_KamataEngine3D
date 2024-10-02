d38ed6c67911f5602ec105e0ab35c4e6528c6fb9
sora_suzuki <k022g0047@g.neec.ac.jp>
2024-10-02 16:28:07 +0900

    移動ベクトルの設定

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -21,6 +21,26 @@ void Player::Update() {
 
 	//行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
+
+	//キャラクターの移動ベクトル
+	Vector3 move = {0, 0, 0};
+
+	//キャラクターの移動する速さ
+	const float kCharacterSpd = 0.2f;
+
+	//押した方向で移動ベクトルを変更（左右）
+	if (input_->PushKey(DIK_LEFT)){
+		move.x -= kCharacterSpd;
+	}else if (input_->PushKey(DIK_RIGHT)){
+		move.x += kCharacterSpd;
+	}
+
+	//押した方向で移動ベクトルを変更（上下）
+	if (input_->PushKey(DIK_UP)) {
+		move.y += kCharacterSpd;
+	} else if (input_->PushKey(DIK_DOWN)) {
+		move.y -= kCharacterSpd;
+	}
 }
 
 void Player::Draw(ViewProjection& viewProjection) {
